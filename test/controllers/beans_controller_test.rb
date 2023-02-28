5b5df576e3141399b435a8e11dd1d6140fb59a86
Marc Mauri Alloza <marc.mauri@codurance.com>
2023-02-28 23:44:35 +0100

    fix: test names

--- test/controllers/beans_controller_test.rb
+++ test/controllers/beans_controller_test.rb
@@ -3,32 +3,32 @@ require "docker"
 
 class BeansControllerTest < ActionDispatch::IntegrationTest
 
-  test "should show cofee beans page" do
+  test "should show coffee beans page" do
     get beans_url
     assert_response :success
   end
 
-  test "should show cofee beans page title" do
+  test "should show coffee beans page title" do
     get beans_url
     assert_select "h1", "Beans"
   end
 
-  test "should show a cofee in a list" do
+  test "should show a coffee in a list" do
     get beans_url
     assert_select "a", "Sumatra Raja Gayo Café de Origen en grano Bio Fairtrade"
   end
 
-  test "should show a cofee in a list with a link" do
+  test "should show a coffee in a list with a link" do
     get beans_url
     assert_select "a[href=?]", "https://alternativa3.com/cafe-en-grano/cafe-grano-origen-sumatra-raja-gayo-bio-ft-500g/"
   end
 
-  test "should show another cofee in a list" do
+  test "should show another coffee in a list" do
     get beans_url
     assert_select "a", "Estanzuela"
   end
 
-  test "should show another cofee in a list with a link" do
+  test "should show another coffee in a list with a link" do
     get beans_url
     assert_select "a[href=?]", "https://syra.coffee/products/estanzuela"
   end
