76a83656aa3ba477f39f280490996b8370fc1e00
Marc Mauri Alloza <marc.mauri@codurance.com>
2023-03-14 12:46:07 +0100

    refactor: remove unneeded parameter to generate taste vector

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -3,9 +3,9 @@ class Coffee < ApplicationRecord
 
     after_save :update_taste_vector
 
-    def generate_taste_vector(all_taste_notes, coffee_taste_notes)
+    def generate_taste_vector(all_taste_notes)
         taste_notes_vector = all_taste_notes.map { |taste_note|
-            next "1" if coffee_taste_notes.any? {|coffee_taste_note| coffee_taste_note == taste_note }
+            next "1" if self.taste_notes.any? {|coffee_taste_note| coffee_taste_note == taste_note }
             "0"
         }
         return "(" + taste_notes_vector.join(",") + ")"
@@ -13,7 +13,7 @@ class Coffee < ApplicationRecord
 
     def update_taste_vector(taste_note = nil)
         all_taste_notes = TasteNote.order('id ASC')
-        taste_vector = self.generate_taste_vector(all_taste_notes, self.taste_notes)
+        taste_vector = self.generate_taste_vector(all_taste_notes)
         sql = "UPDATE coffees SET taste_notes_vector = '" + taste_vector + "' WHERE id = " + self.id.to_s
         ActiveRecord::Base.connection.execute(sql)
     end
--- test/helpers/taste_vector_generator_test.rb
+++ test/helpers/taste_vector_generator_test.rb
@@ -4,21 +4,21 @@ class TasteVectorGeneratorTest < ActiveSupport::TestCase
         coffee = Coffee.new()
         all_taste_notes = []
         taste_notes = []
-        assert "()" == coffee.generate_taste_vector(all_taste_notes, taste_notes)
+        assert "()" == coffee.generate_taste_vector(all_taste_notes)
     end
 
     test "it should generate a single taste note vector if a taste note is received" do
         coffee = Coffee.new()
         all_taste_notes = [TasteNote.new()]
         taste_notes = []
-        assert "(0)" == coffee.generate_taste_vector(all_taste_notes, taste_notes)
+        assert "(0)" == coffee.generate_taste_vector(all_taste_notes)
     end
 
     test "it should generate many taste note vector if many taste notes are received" do
         coffee = Coffee.new()
         all_taste_notes = [TasteNote.new(), TasteNote.new()]
         taste_notes = []
-        assert "(0,0)" == coffee.generate_taste_vector(all_taste_notes, taste_notes)
+        assert "(0,0)" == coffee.generate_taste_vector(all_taste_notes)
     end
 
     test "it should set to one positions of the vector that are taste notes of the coffee" do
@@ -26,7 +26,7 @@ class TasteVectorGeneratorTest < ActiveSupport::TestCase
         coffee = Coffee.create()
         coffee.taste_notes << taste_note
         all_taste_notes = [taste_note, TasteNote.new()]
-        assert "(1,0)" == coffee.generate_taste_vector(all_taste_notes, coffee.taste_notes)
+        assert "(1,0)" == coffee.generate_taste_vector(all_taste_notes)
     end
 
 end
