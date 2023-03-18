dbecd5a35da397bf251a903fa0bec814eb73fab1
Marc Mauri Alloza <marc.mauri@codurance.com>
2023-03-18 23:57:18 +0100

    chore: search by taste notes

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -1,6 +1,7 @@
 class CoffeesController < ApplicationController
 
   def index
+    @taste_notes = TasteNote.all
     if params[:taste_notes].present?
       @coffees = Coffee.query_by_taste_notes(params[:taste_notes])
     else
