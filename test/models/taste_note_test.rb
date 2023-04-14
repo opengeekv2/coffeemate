fb8e4c7545fb61392233bc6924ac96d33f0a762c
Marc Mauri Alloza <marc.mauri@codurance.com>
2023-04-14 19:02:18 +0200

    test: unit test

--- test/models/taste_note_test.rb
+++ test/models/taste_note_test.rb
@@ -29,4 +29,36 @@ class TasteNoteTest < ActiveSupport::TestCase
       row['taste_notes_vector'].count(",") == 1
     }
   end
+
+  test "is_like should be true if a taste is itself" do
+    taste_note = TasteNote.create(name: "Paarent")
+    assert taste_note.is_like?(taste_note)
+  end
+
+  test "is_like should be true if a taste is its parent" do
+    taste_note = TasteNote.create(name: "Parent")
+    taste_note_child = TasteNote.create(name: "ParentChild", parent: taste_note)
+    assert taste_note_child.is_like?(taste_note)
+  end
+
+  test "is_like should be true if a taste is its parents parent" do
+    taste_note = TasteNote.create(name: "Parent")
+    taste_note_child = TasteNote.create(name: "ParentChild", parent: taste_note)
+    taste_note_child_child = TasteNote.create(name: "ParentChildChild", parent: taste_note_child)
+    assert taste_note_child_child.is_like?(taste_note)
+  end
+
+  test "is_like should be false if a taste is not a direct parent" do
+    taste_note = TasteNote.create(name: "Parent")
+    taste_note_child = TasteNote.create(name: "ParentChild", parent: taste_note)
+    taste_note_other_child = TasteNote.create(name: "ParentChild", parent: taste_note)
+    taste_note_child_child = TasteNote.create(name: "ParentChildChild", parent: taste_note_child)
+    assert !taste_note_child_child.is_like?(taste_note_other_child)
+  end
+
+  test "is_like should be false if a taste is unrelated" do
+    taste_note = TasteNote.create(name: "Parent")
+    taste_note_other = TasteNote.create(name: "OtherParent")
+    assert !taste_note_other.is_like?(taste_note)
+  end
 end
